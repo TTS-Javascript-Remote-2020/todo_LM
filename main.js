094aeb8d8dcf57f7fdcb6222bf72d2c80f2570ff
ShowcaseLeslie <49282744+ShowcaseLeslie@users.noreply.github.com>
2020-05-26 15:40:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function todoList() {
+	
+	let item    = document.getElementById('todoInput').value;
+
+	let text    = document.createTextNode(item);
+	
+	let newItem = document.createElement('li');
+	
+	newItem.appendChild(text);
+	 
+	document.getElementById('todoList').appendChild(newItem);
+}
